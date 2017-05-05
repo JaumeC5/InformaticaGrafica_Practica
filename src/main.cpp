7bad257951888abeefc6509cc81389636ad04ae0
Jaume Company <jaume_c_85@hotmail.com>
2017-05-05 02:29:46 +0200

    Model (Not working)

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 #include "Camera.h"
 #include "Shader.h"
+#include "Camera.h"
+#include "Model.h"
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -20,11 +22,6 @@ using namespace glm;
 using namespace std;
 
 const GLint WIDTH = 800, HEIGHT = 600;
-bool textureMove;
-float textOpacity = 0.9;
-float rotationX = 0.0;
-float rotationY = 0.0;
-float rotationZ = 0.0;
 
 GLfloat deltaTime;
 GLfloat lastFrame;
@@ -32,9 +29,6 @@ GLfloat lastFrame;
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void mouseMove(GLFWwindow* window, double xpos, double ypos);
 void mouseScroll(GLFWwindow* window, double xScroll, double yScroll);
-
-
-//bool status[1024];
 
 Camera cam(vec3(0.0f, 0.0f, 3.0f), vec3(0.0f,0.0f,0.0f), 0.03f, 45.0f);
 
@@ -70,226 +64,55 @@ int main() {
 
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-	glEnable(GL_DEPTH_TEST);
-
 	int screenWidth, screenHeight;
 	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
-
 	glViewport(0, 0, screenWidth, screenHeight);
 
-	glClear(GL_COLOR_BUFFER_BIT);
-	glClearColor(0, 0, 0, 0);
+	glEnable(GL_DEPTH_TEST);
 
 	Shader projectShader("./src/projectVertex.vertexshader", "./src/projectFragment.fragmentshader");
-	
-	
-	//Camara----------------------------------------------------------------------------------------------------------------------C
+
+	Model projectModel("./src/spider/spider.obj");
+
 	float aspectRatio = WIDTH / HEIGHT;
-	mat4 model;
-
-
-	//Rotation & Translation-----------------------------------------------------------------------------------------------------R&T
-
-	mat4 translationTrans;
-	translationTrans = glm::translate(translationTrans, vec3(0.0f, 0.0f, 0.0f));
-
-
-	mat4 rotationTransX;
-	mat4 rotationTransY;
-	mat4 rotationTransZ;
-	//rotationTransY = glm::rotate(translationTrans, glm::radians(50.0f), vec3(0.0, 0.0, 1.0f));
-
-	//VAO & VBO------------------------------------------------------------------------------------------------------------------V&V
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	GLuint VBO;
-	glGenBuffers(1, &VBO);
-
-	GLfloat VertexBufferCube[] = {
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		0.5f , -0.5f, -0.5f,  1.0f, 0.0f,
-		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f ,  0.5f,  0.5f,  1.0f, 1.0f,
-		0.5f ,  0.5f,  0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f , -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f , -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f , -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f , -0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-
-	vec3 CubesPositionBuffer[] = {
-		vec3(0.0f ,  0.0f,  0.0f),
-		vec3(2.0f ,  5.0f, -15.0f),
-		vec3(-1.5f, -2.2f, -2.5f),
-		vec3(-3.8f, -2.0f, -12.3f),
-		vec3(2.4f , -0.4f, -3.5f),
-		vec3(-1.7f,  3.0f, -7.5f),
-		vec3(1.3f , -2.0f, -2.5f),
-		vec3(1.5f ,  2.0f, -2.5f),
-		vec3(1.5f ,  0.2f, -1.5f),
-		vec3(-1.3f,  1.0f, -1.5f)
-	};
-	
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexBufferCube), VertexBufferCube, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid*)0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid*)( 3 * sizeof(GL_FLOAT)));
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-
-	//Texturas---------------------------------------------------------------------------------------------------------------Texturas
-	GLuint texture1;
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	int textureWidth, textureHeight;
-	unsigned char* image;
-
-	image = SOIL_load_image("./src/textureCena.png", &textureWidth, &textureHeight, 0, SOIL_LOAD_RGB);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureWidth, textureHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-	SOIL_free_image_data(image);
-
-	GLuint texture2;
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	image = SOIL_load_image("./src/texture.png", &textureWidth, &textureHeight, 0, SOIL_LOAD_RGB);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureWidth, textureHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-	SOIL_free_image_data(image);
-
-	//--
-	GLint moveTex = glGetUniformLocation(projectShader.Program, "opacity");
 
 
 
 	//While-------------------------------------------------------------------------------------------------------------------While
 	while (!glfwWindowShouldClose(window)) {
-	
-		glfwPollEvents();
-		cam.doMovement(window, deltaTime);
-
-		glClear(GL_COLOR_BUFFER_BIT);
-		glClearColor(0.2f, 0.2f, 0.4f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		glBindVertexArray(VAO);
-
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture1);
-		glUniform1i(glGetUniformLocation(projectShader.Program, "Texture1"), 0);
-
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
-		glUniform1i(glGetUniformLocation(projectShader.Program, "Texture2"), 1);
-
-		//Camara view & proyection
-
-		mat4 proj = glm::perspective(glm::radians(cam.GetFOV()), aspectRatio, 1.0f, 100.0f);
-
-
-		//Get uniform location
-		GLint uniView = glGetUniformLocation(projectShader.Program, "view");
-		glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(cam.LookAt()));
-		GLint uniProj = glGetUniformLocation(projectShader.Program, "proj");
-		glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
-		GLint uniMode = glGetUniformLocation(projectShader.Program, "model");
-		glUniformMatrix4fv(uniMode, 1, GL_FALSE, glm::value_ptr(model));
-		
 		//Delta time
 		actualTime = glfwGetTime();
 		deltaTime = actualTime - lastFrame;
 		lastFrame = actualTime;
 
-		//Opacidad texturas
-		if (textureMove) {glUniform1f(moveTex, textOpacity);}
-		else if (!textureMove) {glUniform1f(moveTex, textOpacity);}
+		//Events
+		glfwPollEvents();
+		cam.doMovement(window, deltaTime);
 
-
-		//Cubo central
-		glm::mat4 trans, rot, rotX,  rotY, rotZ;
-		trans = glm::translate(trans, CubesPositionBuffer[0]); 
-		rotX = glm::rotate(rot, glm::radians(rotationX), glm::vec3(1.0f, 0.f, 0.0f)); 
-		rotY = glm::rotate(rot, glm::radians(rotationY), glm::vec3(0.0f, 1.0f, 0.0f)); 
-		rotZ = glm::rotate(rot, glm::radians(rotationZ), glm::vec3(0.0f, 0.f, 1.0f));
-		rot = rotX*rotY*rotZ; 
-		rotZ = glm::rotate(rot, glm::radians(rotationZ), glm::vec3(1.0f, 1.0f, 1.0f));
-		model = trans * rot; 
-		glUniformMatrix4fv(uniMode, 1, GL_FALSE, glm::value_ptr(model)); 
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-
-		//Resto de cubos
-		for (int i = 1; i < 10; i++) {
-			glm::mat4 trans, rot;
-			trans = glm::translate(trans, CubesPositionBuffer[i]);
-			rot = glm::rotate(rot, actualTime *0.1f * glm::radians(180.f), glm::vec3(0.3f, 1.0f, 0.0f));
-			model = trans * rot;
-			glUniformMatrix4fv(uniMode, 1, GL_FALSE, glm::value_ptr(model));
-			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+		//Clear
+		glClearColor(0.2f, 0.2f, 0.4f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		projectShader.USE();
 
-		glBindVertexArray(0);
+		//Camara model, view & proyection
+
+		mat4 proj = glm::perspective(glm::radians(cam.GetFOV()), aspectRatio, 1.0f, 100.0f);
+		GLint uniProj = glGetUniformLocation(projectShader.Program, "proj");
+		glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
+
+		GLint uniView = glGetUniformLocation(projectShader.Program, "view");
+		glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(cam.LookAt()));
+		
+		mat4 model;
+		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f));
+		//model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
+		GLint uniMode = glGetUniformLocation(projectShader.Program, "model");
+		glUniformMatrix4fv(uniMode, 1, GL_FALSE, glm::value_ptr(model));
+		
 		glfwSwapBuffers(window);
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteTextures(1, &texture1);
-	glDeleteTextures(1, &texture2);
 	exit(EXIT_SUCCESS);
 }
 
@@ -299,39 +122,6 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		glfwSetWindowShouldClose(window, GL_TRUE);
 
 	//Opacidad texturas
-	if (key == GLFW_KEY_UP && action == GLFW_PRESS) {
-		textureMove = true;
-		textOpacity += 0.1;
-	}
-	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS) {
-		textureMove = false;
-		textOpacity -= 0.1;
-	}
-	if (key == GLFW_KEY_1) {
-		textOpacity = 1;
-	}
-	if (key == GLFW_KEY_2) {
-		textOpacity = 0;
-	}
-	//Rotacion cubo central
-	if (key == GLFW_KEY_I) {
-		rotationX -= 10.0f;
-	}
-	if (key == GLFW_KEY_K) {
-		rotationX += 10.0f;
-	}
-	if (key == GLFW_KEY_J) {
-		rotationY += 10.0f;
-	}
-	if (key == GLFW_KEY_L) {
-		rotationY -= 10.0f;
-	}
-	if (key == GLFW_KEY_U) {
-		rotationZ += 10.0f;
-	}
-	if (key == GLFW_KEY_O) {
-		rotationZ -= 10.0f;
-	}
 	if (action == GLFW_PRESS) {
 		status[key] = true;
 	}
